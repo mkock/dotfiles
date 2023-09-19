a805372d7e66f945c80bba6b4ef78403533003c2
Martin Kock <marty@pop-os.localdomain>
2023-09-19 10:50:12 +0200

    Updated to use lazy.nvim, also removed leap

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,6 @@
 -- Super basic NeoVim configuration.
--- Author: LuxÆterna. Requirements: NeoVim 0.8.2+, patched fonts, and whatever Telescope likes to have available on the system.
--- Enables: Packer, TokyoNight theme, relative line numbers, indentation with four spaces. Also Lualine and Leap.
-
-local packer = require('packer')
+-- Author: LuxÆterna. Requirements: NeoVim 0.9.0+, patched fonts, and whatever Telescope likes to have available on the system.
+-- Enables: Lazy, TokyoNight theme, relative line numbers, indentation with four spaces. Also Lualine.
 
 -- Options
 vim.opt.number = true
@@ -17,49 +15,51 @@ vim.opt.expandtab = true
 vim.opt.showmode = false
 vim.opt.showmatch = true
 
--- Theme
+-- Lazy.nvim and plugins
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+      lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+    },
+    {
+   	    'nvim-telescope/telescope.nvim', tag = '0.1.3',
+ 	    dependencies = { {'nvim-lua/plenary.nvim'} }
+    },{
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'kyazdani42/nvim-web-devicons', opt = true },
+     }
+
+}
+
+require("lazy").setup(plugins)
+
+-- Enable colorscheme
 vim.cmd[[colorscheme tokyonight]]
 
--- Bootstrapping of Packer and plugins.
-return packer.startup(function(use)
-   -- Packer.
-    use 'wbthomason/packer.nvim'
+-- Enable lualine
+require("lualine").setup()
 
-    -- Theme.
-    use 'folke/tokyonight.nvim'
-
-    -- Telescope.
-    use {
-  	    'nvim-telescope/telescope.nvim', tag = '0.1.0',
-	    requires = { {'nvim-lua/plenary.nvim'} }
-    }
-
-    -- Lualine.
-    use {
-        'nvim-lualine/lualine.nvim',
-        requires = { 'kyazdani42/nvim-web-devicons', opt = true },
-    }
-
-    -- Leap.
-    use 'ggandor/leap.nvim'
-
-    local lualine = require('lualine')
-    local builtin = require('telescope.builtin')
-    local leap = require('leap').add_default_mappings()
- 
-    lualine.setup()
-
-    -- Packer sync.
-    if packer_bootstrap then
-	    require('packer').sync()
-    end
-
-    -- Keymappings
-    vim.g.mapleader = ' '
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-    vim.keymap.set('n', '<leader>c', ':bd<cr>')
-end)
+-- Keymappings
+vim.g.mapleader = ' '
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>ft', builtin.current_buffer_fuzzy_find, {})
 
