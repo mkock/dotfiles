b41a01d09f2b1dc2ee6f44c78c5ba015e6cef84b
Martin Kock <mko@sonarapp.com>
2024-01-05 08:58:40 +0100

    Add commitlint

---
+++ fish/functions/commitlint.fish
@@ -0,0 +1,16 @@
+function commitlint --description 'Displays valid enum types for git commits'
+    echo "[
+  'build',
+  'chore',
+  'ci',
+  'docs',
+  'feat',
+  'fix',
+  'perf',
+  'refactor',
+  'revert',
+  'style',
+  'test'
+];"
+end
+
