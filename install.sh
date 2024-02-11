2fc674f46c44e7a06181da09f414e8b73b59acd9
Martin Kock <kock.martin@gmail.com>
2024-02-11 15:59:21 +0100

    Set opacity in kitty

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir ~/.config/lvim
+mkdir ~/.config/nvim
+mkdir ~/.config/fish
+mkdir ~/.config/kitty
+cp lvim/* ~/.config/lvim/
+cp nvim/* ~/.config/nvim/
+cp fish/* ~/.config/fish/
+cp kitty/* ~/.config/kitty/
+
