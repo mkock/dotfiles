2cc0ea46eed76f9a40964879d18c41d02f4e10b3
Martin Kock <mko@sonarapp.com>
2024-02-23 10:38:32 +0100

    Update install script

--- install.sh
+++ install.sh
@@ -3,8 +3,10 @@ mkdir ~/.config/lvim
 mkdir ~/.config/nvim
 mkdir ~/.config/fish
 mkdir ~/.config/kitty
+mkdir ~/.config/lazygit
 cp lvim/* ~/.config/lvim/
 cp nvim/* ~/.config/nvim/
 cp fish/* ~/.config/fish/
 cp kitty/* ~/.config/kitty/
+cp lazygit/* ~/.config/lazygit/
 
